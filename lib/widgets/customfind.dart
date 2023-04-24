c0095c250dab7dc9490314df22bca924219f853c
Gustavo P <gpedr56@gmail.com>
2023-04-24 12:02:13 -0300

    :sparkles: feat: Widget de campo de endereço

--- lib/widgets/customfind.dart
+++ lib/widgets/customfind.dart
@@ -8,11 +8,8 @@ class CustomFind extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double fem = MediaQuery.of(context).textScaleFactor;
-    final double ffem = fem * 1.0;
-
     return Container(
-      margin: EdgeInsets.fromLTRB(8 * fem, 0 * fem, 8 * fem, 32 * fem),
+      margin: EdgeInsets.fromLTRB(8, 0, 8, 32),
       child: TextButton(
         onPressed: onPressed,
         style: TextButton.styleFrom(
@@ -22,20 +19,19 @@ class CustomFind extends StatelessWidget {
           padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
           child: Container(
             width: double.infinity,
-            height: 56 * fem,
+            height: 56,
             child: Container(
-              padding:
-                  EdgeInsets.fromLTRB(24 * fem, 16 * fem, 16 * fem, 16 * fem),
+              padding: EdgeInsets.fromLTRB(24, 16, 16, 16),
               width: double.infinity,
               height: double.infinity,
               decoration: BoxDecoration(
-                color: Color.fromRGBO(255, 255, 255, 0.612),
-                borderRadius: BorderRadius.circular(16 * fem),
+                color: Color.fromRGBO(255, 255, 255, 0.973),
+                borderRadius: BorderRadius.circular(16),
                 boxShadow: [
                   BoxShadow(
                     color: Color(0x14263f33),
-                    offset: Offset(0 * fem, 8 * fem),
-                    blurRadius: 8 * fem,
+                    offset: Offset(0, 8),
+                    blurRadius: 8,
                   ),
                 ],
               ),
@@ -43,42 +39,39 @@ class CustomFind extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Container(
-                    margin:
-                        EdgeInsets.fromLTRB(0 * fem, 0 * fem, 7 * fem, 0 * fem),
-                    width: 16 * fem,
-                    height: 16 * fem,
+                    margin: EdgeInsets.fromLTRB(0, 0, 7, 0),
+                    width: 20,
+                    height: 24,
                     child: SvgPicture.asset(
                       'assets/search.svg',
-                      width: 16 * fem,
-                      height: 16 * fem,
+                      width: 16,
+                      height: 16,
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.fromLTRB(
-                        0 * fem, 1 * fem, 88 * fem, 1 * fem),
-                    padding:
-                        EdgeInsets.fromLTRB(1 * fem, 0 * fem, 0 * fem, 0 * fem),
+                    margin: EdgeInsets.fromLTRB(0, 1, 88, 1),
+                    padding: EdgeInsets.fromLTRB(1, 0, 0, 0),
                     height: double.infinity,
                     child: Text(
                       'Qual seu destino?',
                       style: TextStyle(
                         fontFamily: 'Inria Sans',
-                        fontSize: 18 * ffem,
+                        fontSize: 18,
                         fontWeight: FontWeight.w400,
-                        height: 1.1975 * ffem / fem,
-                        letterSpacing: 0.18 * fem,
+                        height: 1.1975,
+                        letterSpacing: 0.18,
                         color: Color(0xff000000),
                       ),
                     ),
                   ),
                   Container(
                     margin: EdgeInsets.fromLTRB(8, 1, 1, 1),
-                    width: 48 * fem,
-                    height: 24 * fem,
+                    width: 48,
+                    height: 24,
                     child: SvgPicture.asset(
                       'assets/clock.svg',
-                      width: 24 * fem,
-                      height: 24 * fem,
+                      width: 24,
+                      height: 24,
                     ),
                   ),
                 ],
