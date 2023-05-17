ccfca818376b8326fbc266ace46a985c06e59580
Maísa Moreira <maisamoreira@Maisas-MacBook-Air.local>
2023-05-17 14:47:45 -0300

    resolvendo #44

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,107 +4,109 @@ import 'package:flutter/material.dart';
 import 'package:caronapp/widgets/customtextfield.dart';
 import 'package:caronapp/widgets/custombutton.dart';
 
-// ignore: use_key_in_widget_constructors
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFF5757),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 80, left: 40.0, right: 40.0, bottom: 80.0),
-                    child: Image.asset(
-                      'assets/images/vemjunto.png',
-                      width: 280,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+      body: ListView(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                Align(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const CustomTextField(
-                        labelTextCustom: 'E-mail',
-                        keyboardTypeCustom: TextInputType.emailAddress,
-                        backgroundColorCustom: Colors.white,
-                      ),
-                      const CustomTextField(
-                        labelTextCustom: 'Senha',
-                        keyboardTypeCustom: TextInputType.text,
-                        backgroundColorCustom: Colors.white,
-                      ),
                       Padding(
-                        padding: const EdgeInsets.only(top: 16.0, left: 40.0),
-                        child: TextButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(),
-                          child: const Text(
-                            'Esqueci minha senha',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.normal,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 40.0),
-                        child: CustomButton(
-                          text: 'Login',
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(
-                              builder: (context) => HomeCarona()
-                            )
-                            );
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16.0, left: 40.0),
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context, 
-                              MaterialPageRoute(
-                              builder: (context) => Cadastro()
-                            )
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(),
-                          child: const Text(
-                            'Não tem uma conta? Inscreva-se',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.normal,
-                              fontSize: 12,
-                            ),
-                          ),
+                        padding: const EdgeInsets.only(
+                            top: 80, left: 40.0, right: 40.0, bottom: 80.0),
+                        child: Image.asset(
+                          'assets/images/vemjunto.png',
+                          width: 280,
                         ),
                       ),
                     ],
                   ),
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const CustomTextField(
+                            labelTextCustom: 'E-mail',
+                            keyboardTypeCustom: TextInputType.emailAddress,
+                            backgroundColorCustom: Colors.white,
+                          ),
+                          const CustomTextField(
+                            labelTextCustom: 'Senha',
+                            keyboardTypeCustom: TextInputType.text,
+                            backgroundColorCustom: Colors.white,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(top: 16.0, left: 40.0),
+                            child: TextButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(),
+                              child: const Text(
+                                'Esqueci minha senha',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 40.0),
+                            child: CustomButton(
+                              text: 'Login',
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomeCarona()));
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(top: 16.0, left: 40.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Cadastro()));
+                              },
+                              style: ElevatedButton.styleFrom(),
+                              child: const Text(
+                                'Não tem uma conta? Inscreva-se',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
