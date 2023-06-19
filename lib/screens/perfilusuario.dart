6d83e2559f98e9fda1f8a3c5972c81a282e7c0b8
FelpsFon <felipelfontoura@gmail.com>
2023-06-19 09:20:43 -0300

    Validação de formulário ao alterar dados do usr no aplicativo

--- lib/screens/perfilusuario.dart
+++ lib/screens/perfilusuario.dart
@@ -1,9 +1,11 @@
 import 'package:caronapp/const.dart';
 import 'package:caronapp/screens/atividades.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../widgets/custombutton.dart';
-import '../widgets/customtextfield.dart';
+import '../widgets/formtextfield.dart';
 import '../widgets/roundprofilepicture.dart';
+import '../store/user_store.dart';
 
 class PerfilUsuario extends StatefulWidget {
   const PerfilUsuario({super.key});
@@ -13,6 +15,70 @@ class PerfilUsuario extends StatefulWidget {
 }
 
 class _PerfilUsuario extends State<PerfilUsuario> {
+  final _form = GlobalKey<FormState>();
+  final _celularController = TextEditingController();
+  final _senhaController = TextEditingController();
+
+  String _errorLogin = '';
+
+  UserStore userTeste = UserStore();
+
+  String? _validateCelular(String? value) {
+    if (value == null || value.isEmpty) {
+      return "O número de celular é obrigatório";
+    }
+    if (value.length != 11) {
+      return "O número de celular deve ter exatamente 11 números";
+    }
+    return null;
+  }
+
+  String? _validateSenha(String? value) {
+    if (value == null || value.isEmpty) {
+      return "A senha é obrigatória";
+    }
+    if (value.length < 8) {
+      return "Sua senha deve ter no mínimo 8 caracters";
+    }
+    return null;
+  }
+
+  Future<void> _submitForm(BuildContext context) async {
+    if (!_form.currentState!.validate()) {
+      return;
+    }
+
+    setState(() => _errorLogin = '');
+
+    try {
+      UserStore userStore = Provider.of<UserStore>(context, listen: false);
+      userStore.setCelular(_celularController.text);
+      userStore.setSenha(_senhaController.text);
+
+      Navigator.pushReplacementNamed(context, '/');
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Perfil alterado'),
+            content: const Text('Seu perifl foi alterado com sucesso.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      setState(() => _errorLogin = e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,43 +167,32 @@ class _PerfilUsuario extends State<PerfilUsuario> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const CustomTextField(
-                labelTextCustom: 'Celular',
-                keyboardTypeCustom: TextInputType.phone,
-                backgroundColorCustom: Colors.white,
+              Text(
+                _errorLogin,
+                style: const TextStyle(
+                  height: 2,
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
               ),
-              const CustomTextField(
-                labelTextCustom: 'Matrícula',
-                keyboardTypeCustom: TextInputType.phone,
-                backgroundColorCustom: Colors.white,
-              ),
-              const CustomTextField(
-                labelTextCustom: 'Email',
-                keyboardTypeCustom: TextInputType.phone,
-                backgroundColorCustom: Colors.white,
-              ),
-              const CustomTextField(
-                labelTextCustom: '********',
-                keyboardTypeCustom: TextInputType.phone,
-                backgroundColorCustom: Colors.white,
-              ),
-              const CustomTextField(
-                labelTextCustom: 'Curso',
-                keyboardTypeCustom: TextInputType.phone,
-                backgroundColorCustom: Colors.white,
-              ),
+              FormTextField(
+                controller: _celularController,
+                validator: _validateCelular,
+                keyboardType: TextInputType.phone,
+                labelText: 'Celular'),
+              FormTextField(
+                controller: _senhaController,
+                validator: _validateSenha,
+                keyboardType: TextInputType.text,
+                labelText: 'Senha'),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
                   padding: const EdgeInsets.only(top: 8.0, left: 40.0),
                   child: CustomButton(
                     text: 'Alterar',
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Atividades()));
-                    },
+                    onPressed: () => _submitForm(context),
                   ),
                 ),
               ),
